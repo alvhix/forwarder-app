7efdf9f219d6ec456a44fc23ceb784fc7c158da9
Alvhix <alvhix@gmail.com>
2021-09-19 22:09:20 +0200

    Revert last change: getChats once

--- forwarder/forwarder.py
+++ forwarder/forwarder.py
@@ -186,6 +186,8 @@ class Forwarder:
 
             # user authenticated
             if auth_state["@type"] == self.client.READY:
+                # get all chats
+                self.client.send({"@type": "getChats", "limit": self.limit_chats})
                 self.logger.debug("User authorized")
 
     # handle new messages updates
@@ -198,9 +200,6 @@ class Forwarder:
                 # if the message from chat_id is not from an defined source
                 if message_update["chat_id"] != rule["source"]:
                     continue
-
-                # get all chats
-                self.client.send({"@type": "getChats", "limit": self.limit_chats})
 
                 # build the message
                 message = Message(message_update, rule)
