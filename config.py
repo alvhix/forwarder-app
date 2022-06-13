4d35981bc03dc335079ff679c08bccfa6a1c140c
Alvhix <alvhix@gmail.com>
2022-06-14 01:33:34 +0200

    Updated ignores and config files

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ FORWARDER = {
     "api_id": environ["API_ID"],
     "api_hash": environ["API_HASH"],
     "rules_path": path.join(_dirname, "forwarder-app.config.yml"),
-    "periodicity_fwd": 1,  # second/s
-    "group_messages": False,  # group media messages or not
+    "group_messages": False,  # True to group media messages before forwarding, (it may take $(periodicity_fwd) second/s to forward)
+    "periodicity_fwd": 1,  # second/s (not used if $(group_messages) is false)
     "verbosity": 1,
 }
